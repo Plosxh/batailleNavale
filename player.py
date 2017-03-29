9673767e3cbc922edd4887e51bf5035efc6148a0
PETIT OLIVIER p1613946 <p1613946@univ-lyon1.fr>
2017-03-29 17:49:34 +0200

    get_bateaux

--- player.py
+++ player.py
@@ -1,8 +1,9 @@
 class Player:
 
-    def __init__ (self,nom):
+    def __init__ (self,nom,bateau):
         self._score = 0
         self._nom = nom
+        self._bateaux = bateau
 
     def gagne(self):
         self._score += 1
@@ -16,6 +17,19 @@ class Player:
 
     def get_Nom(self):
         return self._nom
+
+    def get_Bateau(self):
+        return self._bateaux
+
+
+
+
+
+
+
+
+
+
 
 
 
