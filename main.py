e911cef37ed38d68bb611ec64ead72a361e829e6
STEVANT YOUENN p1404008 <p1404008@univ-lyon1.fr>
2017-03-29 16:54:35 +0200

    Ajout bateau

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import unittest
 from Bataille import *
+from Bateau import *
 
 class TestBataille(unittest.TestCase):
 
@@ -8,8 +9,13 @@ class TestBataille(unittest.TestCase):
         jeu = Jeu(nbCase)
         self.assertEqual(False,jeu.get_Grille(1,1))
         self.assertEqual("Error Out Of Bound",jeu.get_Grille(1,nbCase +1))
-        
-        
+
+    def test_Bateau(self):
+        bateau = Bateau(1,2)
+        bateau.est_Touche(0,0)
+        self.assertEqual(False, bateau.est_Coule())
+        bateau.est_Touche(0,1)
+        self.assertEqual(True,bateau.est_Coule())
        
 
 
